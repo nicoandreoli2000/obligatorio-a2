a75a2ffa2dfc2e02283ed35e08a166f62eeb1a00
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-14 18:58:41 -0300

    Feature: Add OpenHash and Map TADS and restructure folders

--- Ejemplo1.java
+++
@@ -1,9 +0,0 @@
-
-import tads.RandomTAD;
-
-public class Ejemplo1 {
-  public static void main(String[] args) {
-    var miTAD = new RandomTAD();
-    miTAD.printHola();
-  }
-}
--- tads/RandomTAD.java
+++
@@ -1,9 +0,0 @@
-package tads;
-
-import java.lang.System;
-
-public class RandomTAD {
-  public void printHola(){
-    System.out.println("HOLA!");
-  }
-}
