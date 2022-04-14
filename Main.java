6591001fada91d5ef281f04aaff1ac76b9b8087a
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-14 20:59:20 -0300

    Feature: Added expection handling in Main.java, need to override .equals

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
-
+import tads.OpenHashMap;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    OpenHashMap<Integer,String> map = new OpenHashMap<Integer, String>(10);
+    map.insert(1, "uno");
+    map.insert(2, "dos");
+    map.insert(3, "tres");
+
+    try{
+      System.out.println("key 1: " + map.get(1));
+      System.out.println("key 2: " + map.get(2));
+      System.out.println("key 3: " + map.get(3));
+    } catch(Exception e) {
+      System.out.println("ERROR: " + e.getMessage());
+    }
   }
 }
