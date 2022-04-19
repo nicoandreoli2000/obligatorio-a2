145c95fb8f7ae246e1d15e34309c4218c6bed609
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-18 21:55:51 -0300

    Fix: Trying to compile everything

---
+++ Ejercicio1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import tads.OpenHashMap;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        try (var in = new Scanner(System.in)) {
+            Integer n = Integer.parseInt(in.nextLine());
+            String s = in.nextLine();
+            System.out.println(ejercicio1(n,s));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Integer ejercicio1(Integer n, String s) {
+        var hash = new OpenHashMap<Integer, Integer>(n);
+        int amount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            var pos = (int) s.charAt(i);
+            if (i % 2 == 0) {
+                if (!hash.contains(pos)) {
+                    amount++;
+                }
+            } else {
+                hash.insert(pos, 1);
+            }
+        }
+        return amount;
+    }
+}
