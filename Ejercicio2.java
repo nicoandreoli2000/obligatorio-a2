aad84f785a51612db29c4adb563bf8065dad89c1
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-26 01:08:30 -0300

    update

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -6,21 +6,19 @@ public class Ejercicio2 {
     public static void main(String[] args) throws Exception {
         var in = new Scanner(System.in);
         Integer n = Integer.parseInt(in.nextLine());
-        System.out.println(ejercicio2(n, in));
+        // System.out.println(ejercicio2(n, in));
         in.close();
     }
 
-    public static String ejercicio2(Integer n, Scanner in) throws Exception {
+    public static String ejercicio2(Integer n, String[] s) throws Exception {
         var hash = new OpenHashMap<String, Integer>(n);
         int maxAmountOfPoints = 0;
         String personWithMaxAmountOfPointsFirst = "";
         for (int i = 0; i < n; i++) {
-            String line = in.nextLine();
+            String line = s[i];
             String[] arr = line.split(" ");
             String name = arr[0];
             int points = Integer.parseInt(arr[1]);
-            System.out.print(name);
-            System.out.print(points);
 
             if (hash.contains(name)) {
                 int prevPoints = hash.get(name);
