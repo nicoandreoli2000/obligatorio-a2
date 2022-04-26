2bb8ddf88db55ca2696b8b514757f081dafa1e19
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-26 20:59:17 -0300

    update

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -4,22 +4,15 @@ import tads.AVL;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        try (Scanner in = new Scanner(System.in)) {
-            Integer k = Integer.parseInt(in.nextLine());
-            Integer size = Integer.parseInt(in.nextLine());
-            AVL<Integer> a = new AVL<Integer>();
-            // while(in.hasNext()){
-            // Integer next = Integer.parseInt(in.nextLine());
-            // a.insert(next);
-            // }
-            for (int i = 0; i < size; i++) {
-                Integer next = Integer.parseInt(in.nextLine());
-                a.insert(next);
-            }
-            ejercicio3(k, a);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
+        Scanner in = new Scanner(System.in);
+        Integer k = Integer.parseInt(in.nextLine());
+        Integer size = Integer.parseInt(in.nextLine());
+        AVL<Integer> a = new AVL<Integer>();
+        for (int i = 0; i < size; i++) {
+            Integer next = Integer.parseInt(in.nextLine());
+            a.insert(next);
         }
+        ejercicio3(k, a);
     }
 
     public static void ejercicio3(Integer k, AVL<Integer> l) {
