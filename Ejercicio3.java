b5bfdea27d7c681739de9a19464e8b5dadc8c48d
Seba <sebaesc01@gmail.com>
2022-04-26 17:53:43 -0300

    “Ejericicio3”

---
+++ Ejercicio3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import tads.AVL;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            Integer k = Integer.parseInt(in.nextLine());
+            Integer size = Integer.parseInt(in.nextLine());
+            AVL<Integer> a = new AVL<Integer>();
+            // while(in.hasNext()){
+            // Integer next = Integer.parseInt(in.nextLine());
+            // a.insert(next);
+            // }
+            for (int i = 0; i < size; i++) {
+                Integer next = Integer.parseInt(in.nextLine());
+                a.insert(next);
+            }
+            ejercicio3(k, a);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void ejercicio3(Integer k, AVL<Integer> l) {
+        for (int i = 0; i < k; i++) {
+            Integer max = l.maximo();
+            System.out.println(max);
+            l.delete(max);
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import tads.*;
+
 public class Main {
     public static void main(String[] args) {
-        Ejercicio1.ejercicio1(10, "iLcPcKlRyCbCbJnBpB");
+        //Ejercicio1.ejercicio1(10, "iLcPcKlRyCbCbJnBpB");
+        try (Scanner in = new Scanner(System.in)) {
+            Integer k = Integer.parseInt(in.nextLine());
+            Integer size = Integer.parseInt(in.nextLine());
+            AVL<Integer> a = new AVL<Integer>();
+            // while(in.hasNext()){
+            // Integer next = Integer.parseInt(in.nextLine());
+            // a.insert(next);
+            // }
+            for (int i = 0; i < size; i++) {
+                Integer next = Integer.parseInt(in.nextLine());
+                a.insert(next);
+            }
+            ejercicio3(k, a);
+            // a.inOrderPrint();
+        } catch (NumberFormatException e) {
+            
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void ejercicio3(Integer k, AVL<Integer> l) {
+        for (int i = 0; i < k; i++) {
+            Integer max = l.maximo();
+            System.out.println(max);
+            l.delete(max);
+        }
     }
 }
