53a9c8682477544d3b41162b438c12724e53b08f
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-05-14 17:08:28 -0300

    Feature: logic for ex. 5

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -3,9 +3,24 @@ import tads.Grafo;
 public class Ejercicio5 {
     public static void main(String[] args) {
         Grafo<Integer> g = Grafo.convertFromText();
-        ejercicio5(g, 1);
+        ejercicio5(g);
     }
 
-    public static void ejercicio5(Grafo<Integer> g, Integer vertice) {
+    public static void ejercicio5(Grafo<Integer> g) {
+        Boolean[] visited = new Boolean[g.getSize()];
+        int counter = 0;
+        for (int i = 0; i < visited.length; i++) {
+            // elimino vertice y adjacentes
+            if (esConexo(g)) {
+                counter++;
+            }
+            // agrego vertice y adjacentes
+            visited[i] = true;
+        }
+        System.out.println(counter);
+    }
+
+    private static boolean esConexo(Grafo<Integer> g) {
+        return true;
     }
 }
