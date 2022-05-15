916e847f35ae40603029752bdb5d89afb0f49503
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-05-15 11:00:18 -0300

    Working on it

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -5,20 +5,20 @@ import tads.AVL;
 public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Integer k = Integer.parseInt(in.nextLine());
-        Integer size = Integer.parseInt(in.nextLine());
+        int k = Integer.parseInt(in.nextLine());
+        int size = Integer.parseInt(in.nextLine());
         AVL<Integer> a = new AVL<Integer>();
         for (int i = 0; i < size; i++) {
-            Integer next = Integer.parseInt(in.nextLine());
+            int next = Integer.parseInt(in.nextLine());
             a.insert(next);
         }
         ejercicio3(k, a);
         in.close();
     }
 
-    public static void ejercicio3(Integer k, AVL<Integer> l) {
+    public static void ejercicio3(int k, AVL<Integer> l) {
         for (int i = 0; i < k; i++) {
-            Integer max = l.maximo();
+            int max = l.maximo();
             System.out.println(max);
             l.delete(max);
         }
--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,69 +1,29 @@
-import java.util.Arrays;
 
 import tads.*;
 
 public class Ejercicio4 {
 
     public static void main(String[] args) {
-        Grafo<Integer> g = Grafo.convertFromText();
-        ejercicio4(g, 1);
+        GraphWithList g = GraphWithList.createUndirectedGraphFromInput();
+        ejercicio4(g);
     }
 
-    public static void ejercicio4(Grafo<Integer> g, Integer vertice) {
-        Boolean[] visitados = new Boolean[g.getSize()];
-        Boolean[] pase = new Boolean[g.getSize()];
-        Arrays.fill(visitados, false);
-        Arrays.fill(pase, false);
-        visitados[vertice - 1] = true;
-        boolean entro2 = false;
-        boolean termino = false;
-        Integer conexos = 1;
-        Integer aux = -1;
-        for (int h = 0; h < visitados.length && !termino; h++) {
-            if (entro2) {
-                h = aux;
-                entro2 = false;
-                visitados[h] = true;
-            }
-            Boolean entro = false;
-            for (int i = 0; i < visitados.length && !entro; i++) {
-                Boolean[] copiaV = Grafo.createCopy(visitados);
-                for (int j = 0; j < visitados.length; j++) {
-                    // verifico que sea un vertice adyacente a uno ya utilizado y que el mismo
-                    // no le haya agregado sus adyacentes
-                    if (visitados[j] && !pase[j]) {
-                        Object[] adys = g.getAdys(j + 1);
-                        pase[j] = true;
-                        if (adys != null) {
-                            for (int k = 0; k < adys.length && adys[k] != null; k++) {
-                                // añado los vertices adyacentes a visitados
-                                Pair<Integer, Integer> pair = (Pair<Integer, Integer>) adys[k];
-                                visitados[pair.fst() - 1] = true;
-                            }
-                        }
-
-                    }
-                }
-                if (Arrays.equals(visitados, copiaV)) {
-                    entro = true;
-                    // lo uso para saber si el array fue modificado,
-                    // sirve para ver si empezaria a repetir vertices
-                }
-            }
-            for (int u = 0; u < visitados.length && !entro2; u++) {
-                if (visitados[u] == false) {
-                    aux = u;
-                    conexos++;
-                    entro2 = true;
-                    // lo uso para ver si entro en este for, si es asi guardo
-                    // con que valor entro para usarlo luego
-                }
-            }
-            if (Grafo.allTrue(visitados)) {
-                termino = true;
+    public static void ejercicio4(GraphWithList g) {
+        boolean[] visited = new boolean[g.getSize()];
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = false;
+        }
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < visited.length; i++) {
+            if (g.containsEdge(i)) {
+                stack.push(i);
+                break;
             }
         }
+        int counter = 0;
+        while (!stack.isEmpty()) {
 
-        System.out.println(conexos);
+        }
+        System.out.println(counter);
     }
 }
