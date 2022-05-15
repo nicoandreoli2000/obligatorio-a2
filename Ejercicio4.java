7ae6c5ad34b94cdeec3fbc40f2f03bbc82780acb
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-05-15 16:41:58 -0300

    Finished

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -12,8 +12,10 @@ public class Ejercicio4 {
 
     public static void ejercicio4(GraphWithList g) throws Exception {
         boolean[] visited = new boolean[g.getSize()];
+        boolean[] stacked = new boolean[g.getSize()];
         for (int i = 0; i < visited.length; i++) {
             visited[i] = false;
+            stacked[i] = false;
         }
         Stack<Integer> stack = new Stack<Integer>();
         int amountVisited = 0;
@@ -22,24 +24,22 @@ public class Ejercicio4 {
             if (stack.isEmpty()) {
                 counter++;
                 for (int i = 0; i < visited.length; i++) {
-                    if (!visited[i]) {
+                    if (!stacked[i] && !visited[i]) {
                         stack.push(i);
                         break;
                     }
                 }
             } else {
                 int elem = stack.pop();
-                if (!visited[elem]) {
-
-                    visited[elem] = true;
-                    amountVisited++;
-                    List<Integer> adjs = g.getEdges(elem);
-                    Iterator<Integer> it = adjs.iterator();
-                    while (it.hasNext()) {
-                        int edge = it.next();
-                        if (!visited[edge]) {
-                            stack.push(edge);
-                        }
+                visited[elem] = true;
+                amountVisited++;
+                List<Integer> adjs = g.getEdges(elem);
+                Iterator<Integer> it = adjs.iterator();
+                while (it.hasNext()) {
+                    int edge = it.next();
+                    if (!visited[edge] && !stacked[edge]) {
+                        stack.push(edge);
+                        stacked[edge] = true;
                     }
                 }
             }
