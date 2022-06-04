45cb2828de8d0326051baf9936de706117affd35
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-06-04 19:52:07 -0300

    update

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,10 +1,14 @@
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 import tads.DisjointSet;
+import tads.GraphWithList;
+import tads.List;
 import tads.MaxHeap;
 import tads.Pair;
 import tads.PriorityQueue;
+import tads.Stack;
 
 public class Ejercicio7 {
     public static void main(String[] args) throws Exception {
@@ -31,4 +35,5 @@ public class Ejercicio7 {
         }
         System.out.print(costo);
     }
+
 }
