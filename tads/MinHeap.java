caaf01f3a324db82de37ab37a525d76e49a4bb2c
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-06-09 20:36:36 -0300

    min heap

--- tads/MinHeap.java
+++ tads/MinHeap.java
@@ -1,98 +1,40 @@
 package tads;
 
 public class MinHeap<E> implements PriorityQueue<E> {
-    private Object[] arr;
-    private int size;
+    private MaxHeap<E> heap;
 
     public MinHeap(int maxSize) {
-        arr = new Object[maxSize + 1];
-        size = 0;
+        heap = new MaxHeap<E>(maxSize);
     }
 
     @Override
     public void push(E elem, int prio) throws Exception {
-        if (isFull()) {
-            throw new Exception("El Heap está lleno");
-        }
-        arr[size++] = new Pair<E, Integer>(elem, prio);
-        doFloat(size - 1);
+        heap.push(elem, -prio);
     }
 
     @Override
     public void pop() {
-        if (isEmpty()) {
-            return;
-        }
-        arr[0] = arr[size - 1];
-        size--;
-        doSink(0);
+        heap.pop();
     }
 
     @Override
     public Pair<E, Integer> top() throws Exception {
-        if (isEmpty()) {
-            throw new Exception("El Heap está vacío");
-        }
-        return (Pair<E, Integer>) arr[0];
+        return heap.top();
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return heap.isEmpty();
     }
 
     @Override
     public boolean isFull() {
-        return size == arr.length;
+        return heap.isFull();
     }
 
     @Override
     public int size() {
-        return size;
+        return heap.size();
     }
 
-    private void doSink(int i) {
-        Pair<E, Integer> temp = (Pair<E, Integer>) arr[i];
-        while (kthChild(i, 1) < size) {
-            int c = minChild(i);
-            Pair<E, Integer> child = (Pair<E, Integer>) arr[c];
-            if (temp.value > child.value) {
-                arr[i] = child;
-            } else {
-                break;
-            }
-            i = c;
-        }
-        arr[i] = temp;
-    }
-
-    private void doFloat(int i) {
-        Pair<E, Integer> temp = (Pair<E, Integer>) arr[i];
-        while (i > 0 && temp.value > ((Pair<E, Integer>) arr[parent(i)]).value) {
-            arr[i] = arr[parent(i)];
-            i = parent(i);
-        }
-        arr[i] = temp;
-    }
-
-    private int parent(int i) {
-        return (i - 1) / 2;
-    }
-
-    private int kthChild(int i, int k) {
-        return 2 * i + k;
-    }
-
-    private int minChild(int i) {
-        int leftChild = kthChild(i, 1);
-        int rightChild = kthChild(i, 2);
-        return ((Pair<E, Integer>) arr[leftChild]).value < ((Pair<E, Integer>) arr[rightChild]).value ? leftChild
-                : rightChild;
-    }
-
-    public void print() {
-        for (int i = 0; i < size; i++) {
-            System.out.println(arr[i].toString());
-        }
-    }
 }
