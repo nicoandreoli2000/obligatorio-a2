0e67f1fbc63b160301e1e77a797a031e6ee74fc6
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-06-11 19:29:57 -0300

    update

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -34,19 +34,18 @@ public class Ejercicio10 {
 
     public static boolean[][] subset(int set[], int n, int cota) {
 
-        boolean subset[][] = new boolean[cota + 1][n + 1];
+        boolean subset[][] = new boolean[cota + 1][n]; // +1 para incluir el cero
 
-        for (int i = 0; i <= n; i++)
+        for (int i = 0; i < n; i++) {
             subset[0][i] = true;
-
-        for (int i = 1; i <= cota; i++)
-            subset[i][0] = false;
+        }
 
         for (int i = 1; i <= cota; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j < n; j++) {
                 subset[i][j] = subset[i][j - 1];
-                if (i >= set[j - 1])
+                if (i >= set[j - 1]) {
                     subset[i][j] = subset[i][j] || subset[i - set[j - 1]][j - 1];
+                }
             }
         }
 
