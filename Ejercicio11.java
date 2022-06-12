19ffe847fc094f4b7c12df5013a5baf5ccce6f36
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-06-12 12:35:53 -0300

    update

---
+++ Ejercicio11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio11 {
+    public static void main(String[] args) throws Exception {
+        ejercicio11();
+    }
+
+    public static void ejercicio11() throws Exception {
+        Scanner in = new Scanner(System.in);
+        in.close();
+    }
+}
