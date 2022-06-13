d3170c71fb3a849a0ace0628f5a772fded7acf48
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-06-12 21:47:13 -0300

    Finished ej9

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -4,64 +4,28 @@ import java.util.Scanner;
 public class Ejercicio9 {
 
     public static void main(String[] args) {
-        ejercicio9b();
+        ejercicio9();
     }
 
     private static void ejercicio9() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        long[] dp = new long[n+1];
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = dp[i - 1] * i;
-        }
-        int p = in.nextInt();
-        String p2 = in.nextLine();
-        int a = 0;
-        while(in.hasNext()){
-            String line = in.nextLine();
-            String[] arr = line.split(" ");
-            int n1 = Integer.parseInt(arr[0]);
-            int k = Integer.parseInt(arr[1]);
-            long res = dp[n1]/(dp[k]*(dp[n1-k]));
-            //long res2 = 
-            System.out.println(res);
-            a++;
-        }
-        in.close();
-    }
-
-    private static void ejercicio9b() {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        BigInteger[] dp = new BigInteger[n+1];
+        BigInteger[] dp = new BigInteger[n + 1];
         dp[0] = BigInteger.ONE;
         for (int i = 1; i <= n; i++) {
             dp[i] = dp[i - 1].multiply(BigInteger.valueOf(i));
         }
-        int p = in.nextInt();
-        String p2 = in.nextLine();
-        int a = 0;
-        while(in.hasNext()){
+        in.nextLine();
+        in.nextLine();
+        while (in.hasNext()) {
             String line = in.nextLine();
             String[] arr = line.split(" ");
             int n1 = Integer.parseInt(arr[0]);
             int k = Integer.parseInt(arr[1]);
-            BigInteger res = dp[n1].divide(dp[k].multiply(dp[n1-k]));
+            BigInteger res = dp[n1].divide(dp[k].multiply(dp[n1 - k]));
             System.out.println(res);
-            a++;
         }
         in.close();
-    }
-
-    int fibonacci(int n) {
-        int[] tab = new int[n + 1];
-        tab[0] = 0;
-        tab[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            tab[i] = tab[i - 1] + tab[i - 2];
-        }
-        return tab[n];
     }
 
 }
